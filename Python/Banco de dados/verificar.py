24a59f4f6e1018a4fee2e05e547ccb98319145fd
monteiro393 <andre.nascimento393@gmail.com>
2023-03-22 00:29:53 -0300

    def() de carregar o arquivo e do input inseridos

--- Python/Banco de dados/verificar.py
+++ Python/Banco de dados/verificar.py
@@ -1,3 +1,36 @@
+import os
+
+os.chdir(".\Python\Banco de dados") ## Muda o diretório para a pasta onde contém os arquivos .txt
+
+'''
+diretorio_atual = os.getcwd()   ## Teste para saber se o diretório atual está correto
+print(diretorio_atual)
+'''
+
+def carrega_email():
+    with open("email.txt", "r") as arquivo_email_r:
+        emails = []
+
+        for linha in arquivo_email_r:   ## definir uma variável 'linha' e fazer um loop para "ler" o arquivo
+                linha = linha.strip()   ## remover os espaços e \n das palavras no arquivo
+                emails.append(linha)  ## adicionar cada palavra na lista de palavras 
+                ## print(emails)
+
+def input_login():  ## Input do login
+    login = input("> Insira seu usuário ou email: ")    ## Input do login
+    login = login.strip().upper()   ## remove espaços e coloca em letras maiúsculas
+    print()
+    return login
+
+def erro_login():  ## Input para caso erro do login
+    login = input("> Erro, insira seu usuário ou email novamente: ")    ## Input do login
+    login = login.strip().upper()   ## remove espaços e coloca em letras maiúsculas
+    print()
+    return login
+
+## def verifica_se_email(carrega_email, input_login, erro_login):
+ 
+
 alternativas = [1, 2]
 
 print("> Você já possui login?")
@@ -9,5 +42,6 @@ escolha = int(input("> "))
 while escolha not in alternativas:
     escolha = int(input("> Erro, escolha novamente: "))
 
-## WIP
+carrega_email()
 
+## WIP
